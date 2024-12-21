52179681377ea931ef9ea79414bd658b783e9878
AlexisAndradeDev <alexis.martinez.contacto@gmail.com>
2024-12-21 16:52:09 -0600

    Model validation

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,15 +1,21 @@
-﻿namespace Restaurants.Application.Restaurants.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants.Application.Restaurants.Dtos;
 
 public class CreateRestaurantDto
 {
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "String must be between 3 and 100 characters long.")]
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
+    [EmailAddress(ErrorMessage = "Enter a valid email.")]
     public string? ContactEmail { get; set; }
+    [Phone(ErrorMessage = "Enter a valid phone number.")]
     public string? ContactNumber { get; set; }
 
     public string? City { get; set; }
     public string? Street { get; set; }
+    [RegularExpression(@"^\d{2}-\d{3}", ErrorMessage = "Enter a valid postal code using the following format: XX-XXX.")]
     public string? PostalCode { get; set; }
 }
