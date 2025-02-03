40440a65da2ff63fee3915c0ef0c6279c792b96e
AlexisAndradeDev <alexis.martinez.contacto@gmail.com>
2025-02-03 17:07:17 -0600

    Authenticate all restaurants endpoints
    * Extract presentation layer services to extensions

--- Restaurants.API/Controllers/RestaurantsControllers.cs
+++ Restaurants.API/Controllers/RestaurantsControllers.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Restaurants;
 using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
@@ -11,10 +12,12 @@ using Restaurants.Application.Restaurants.Queries.GetRestaurant;
 namespace Restaurants.API.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("api/restaurants")]
 public class RestaurantsControllers(IMediator mediator): ControllerBase
 {
     [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<RestaurantDto>>> GetAll()
     {
         var restaurants = await mediator.Send(new GetAllRestaurantsQuery());
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -5,46 +5,15 @@ using Serilog;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
 using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(config =>
-{
-    config.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    config.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "bearerAuth"
-                }
-            },
-            []
-        }
-    });
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
+builder.AddPresentation();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration)
-);
 
 var app = builder.Build();
 
